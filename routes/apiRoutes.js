ef101fc6ca67e0a857d83d70759a0260381f1431
Jeckfeldt <jeckfeldt123@gmail.com>
2021-06-17 15:35:20 -0700

    setup html routes for server

---
+++ routes/apiRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router()
+const { join } = require('path')
+
+//routes
+router.get()
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const { join } = require('path')
-const fs = require('fs')
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const app = express()
 
+//link css and js to html in public
 app.use(express.static(join(__dirname, 'public')))
+
+//setup data parsing
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-//routes will go here
+//route links
+app.use('/', htmlRoutes)
+
+//start app
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
